d850ed74dd043e1a6311804038ad3444fe8a8237
Patrick <patrick.batke@web.de>
2025-04-23 20:00:57 +0200

    fix video upload

--- backend_hub/settings.py
+++ backend_hub/settings.py
@@ -195,7 +195,7 @@ RQ_QUEUES = {
         'HOST': 'localhost',
         'PORT': 6379,
         'DB': 0,
-        'PASSWORD': 'foobared',
+        # 'PASSWORD': 'foobared',
         'DEFAULT_TIMEOUT': None,
     }
 }
